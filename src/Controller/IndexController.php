0181c7260ea3ab9e322ab01412d713173d81fc9c
Juan Carlos Carrera <carrera1995@hotmail.es>
2019-07-12 01:12:58 -0500

    route admin and index ready and install twig

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,18 +2,42 @@
 // src/Controller/IndexController.php
 namespace App\Controller;
 
+use Symfony\Bunle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class IndexController
+
+
+class IndexController extends AbstractController
 {
+
     public function homepage()
     {
         /**
          * @Route("/")
          */
+        return new Response('<html><body>Hi, i am the home page </body></html>');
+    }
+ 
+   
+    
+    public function adminpage()
+    {
+        /**
+         * @Route("/admin")
+         */ 
+        
+        $comments = [
+            'holix1','holix2','holix3'
+        ];         
 
-        return new Response(
-            '<html><body>Hi, i am the home page </body></html>'
-        );
+        
+        
+        return $this->render('adminpages/home.html.twig',[
+            'title' => ucwords('notitle'),
+            'comments' => $comments,
+        ]);
+        
+       
     }
 }
